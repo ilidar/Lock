adcb6fda16ef54ff3c1643734068ac921e0b7501
Denys Kotelovych <denys.kotelovych@symphony-solutions.eu>
2013-11-21 11:51:20 +0200

    Added right way of filling gradient;

--- Lock/DKViewController.m
+++ Lock/DKViewController.m
@@ -8,10 +8,10 @@
 
 #import "DKViewController.h"
 
-static const CGFloat sDotRadius = 2.0f;
-static const CGFloat sDotMargin = 4.0f;
+static const CGFloat sDotRadius = 5.0f;
+static const CGFloat sDotMargin = 7.5f;
 static const CGFloat sSurfaceRadius = 150.0f;
-static const CGFloat sTouchCircleRadius = 50.0f;
+static const CGFloat sTouchCircleRadius = 70.0f;
 
 typedef struct {
   CGFloat radius;
@@ -34,7 +34,7 @@ static inline BOOL sCircleIntersectsCircle(Circle first, Circle second)
   CGFloat dy = second.center.y - first.center.y;
   CGFloat dist = sqrt(dx * dx + dy * dy);
   
-  return dist <= first.radius + second.radius;
+  return dist <= first.radius + second.radius - 4.0f;
 }
 
 static inline CGFloat sGetSurfaceRadiusChange()
@@ -109,64 +109,54 @@ static inline CGPoint sGetDotCenter(CGFloat angle, CGPoint beginPoint, CGPoint c
 #pragma mark - Draw Methods
 
 - (void)drawRect:(CGRect)rect {
-  for (CGFloat radius = sGetSurfaceRadiusChangeBegin(); radius <= sSurfaceRadius; radius += sGetSurfaceRadiusChange()) {
+  CGFloat colors [] = {
+    1.0, 1.0, 1.0, 0.3,
+    0.0, 0.0, 0.0, 0.7
+  };
+  
+  CGContextRef context = UIGraphicsGetCurrentContext();
+  
+  CGColorSpaceRef baseSpace = CGColorSpaceCreateDeviceRGB();
+  
+  CGFloat glossLocations[] = {0.05, 0.9};
+  
+  CGGradientRef gradient = CGGradientCreateWithColorComponents(baseSpace, colors, glossLocations, 2);
+  
+  CGContextSaveGState(context);
+  
+  CGContextSetLineWidth(context, 2.0);
+  
+  for (CGFloat coef = 1.0f, radius = sGetSurfaceRadiusChangeBegin(); radius <= sSurfaceRadius; radius += sGetSurfaceRadiusChange(), coef += 0.05f) {
     CGPoint beginPoint = CGPointMake(radius, 0.0f);
     
     CGFloat angleChange = sGetArcChange(radius);
     
     for (CGFloat alpha = 0.0f; alpha <= 360.0f; alpha += angleChange) {
-      Circle circle = sCircleMake(sGetDotCenter(alpha, beginPoint, self.center), sDotRadius);
+      Circle circle = sCircleMake(sGetDotCenter(alpha, beginPoint, self.center), sDotRadius / coef);
       
       if (sCircleIntersectsCircle(self.touchCircle, circle)) {
-        [self pm_drawCircle:circle color:[UIColor redColor]];
-      } else {
-        [self pm_drawCircle:circle color:[UIColor blueColor]];
+        CGRect rectangle = CGRectMake(circle.center.x - circle.radius / 2.0f,
+                                      circle.center.y - circle.radius / 2.0f,
+                                      circle.radius,
+                                      circle.radius);
+        
+        CGContextAddEllipseInRect(context, rectangle);
       }
     }
   }
-}
-
-#pragma mark - Private Methods
-
-- (void)pm_drawCircle:(Circle)circle color:(UIColor *)color {
-  CGContextRef context = UIGraphicsGetCurrentContext();
-  
-  CGContextSetLineWidth(context, 2.0);
-  
-  CGContextSetFillColorWithColor(context, color.CGColor);
-  
-  CGRect rectangle = CGRectMake(circle.center.x - circle.radius / 2.0f,
-                                circle.center.y - circle.radius / 2.0f,
-                                circle.radius,
-                                circle.radius);
-  
-  CGContextAddEllipseInRect(context, rectangle);
-  
-  CGContextFillPath(context);
-}
-
-- (void)pm_drawGradientCircle:(Circle)circle {
-  CGFloat colors [] = {
-    0.2, 0.2, 0.2, 1.0,
-    0.0, 0.0, 0.0, 1.0
-  };
-  
-  CGColorSpaceRef baseSpace = CGColorSpaceCreateDeviceRGB();
-  
-  CGGradientRef gradient = CGGradientCreateWithColorComponents(baseSpace, colors, NULL, 2);
-  
-  CGColorSpaceRelease(baseSpace), baseSpace = NULL;
-  
-  CGContextRef context = UIGraphicsGetCurrentContext();
-  
-  CGContextSaveGState(context);
   
   CGContextClip(context);
   
-  CGContextDrawRadialGradient(context, gradient, circle.center, 0, circle.center, circle.radius, kCGGradientDrawsAfterEndLocation);
+  CGContextDrawRadialGradient(context,
+                              gradient,
+                              self.touchCircle.center,
+                              0,
+                              self.touchCircle.center,
+                              self.touchCircle.radius,
+                              kCGGradientDrawsBeforeStartLocation);
   
-  CGGradientRelease(gradient), gradient = NULL;
-
+  CGGradientRelease(gradient);
+  
   CGContextRestoreGState(context);
 }
 
@@ -177,7 +167,7 @@ static inline CGPoint sGetDotCenter(CGFloat angle, CGPoint beginPoint, CGPoint c
 - (void)loadView
 {
   self.view = [[LockView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-  self.view.backgroundColor = [UIColor whiteColor];
+  self.view.backgroundColor = [UIColor blackColor];
 }
 
 @end
